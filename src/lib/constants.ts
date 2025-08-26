393d5c3517d57ad8bebef50dccbe97c7bfb2f26d
Aurelien Brabant <aurelien@brabant.dev>
2025-08-27 00:52:09 +0200

    feat: add discord button

--- src/lib/constants.ts
+++ src/lib/constants.ts
@@ -1,4 +1,5 @@
 export const VICINAE_GITHUB_REPO_URL = "https://github.com/vicinaehq/vicinae";
+export const VICINAE_DISCORD_URL = "https://discord.com/invite/rP4ecD42p7";
 export const VICINAE_GITHUB_ORG_URL = "https://github.com/vicinaehq";
 export const FEATURE_REQUEST_URL = "https://github.com/vicinaehq/vicinae/issues/new?template=feature_request.md";
 export const VICINAE_GENERAL_SETTINGS_URL = "vicinae://settings/open?tab=general";
