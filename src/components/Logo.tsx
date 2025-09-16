78eba02635a142c2f01b7a6f4a0d487839da9088
cilginc <104676201+cilginc@users.noreply.github.com>
2025-09-17 01:14:14 +0300

    chore: updated logo to use next/image instead of <img>

--- src/components/Logo.tsx
+++ src/components/Logo.tsx
@@ -1,8 +1,10 @@
+import Image from "next/image";
+
 export function Logo(props: React.ComponentPropsWithoutRef<'svg'>) {
 	return (
 		<>
-			<img src="/vicinae-dark.svg" className="h-[40px] hidden dark:block" />
-			<img src="/vicinae.svg" className="h-[40px] dark:hidden fill-black" />
+			<Image src="/vicinae-dark.svg" alt="logo-dark" width={40} height={40} className="h-[40px] hidden dark:block" />
+			<Image src="/vicinae.svg" alt="Logo" width={40} height={40} className="h-[40px] dark:hidden fill-black" />
 		</>
 	);
 }
