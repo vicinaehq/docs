5b89e279e8ce7ff2026a8df9f4cbebaf7874aec4
Aurelien Brabant <gh@aurelle.dev>
2025-11-12 20:15:28 +0100

    feat: improve dmenu documentation

--- src/components/Navigation.tsx
+++ src/components/Navigation.tsx
@@ -14,325 +14,325 @@ import { remToPx } from '@/lib/remToPx'
 import { CloseButton } from '@headlessui/react'
 
 interface NavGroup {
-  title: string
-  links: Array<{
-    title: string
-    href: string
-  }>
+	title: string
+	links: Array<{
+		title: string
+		href: string
+	}>
 }
 
 function useInitialValue<T>(value: T, condition = true) {
-  let initialValue = useRef(value).current
-  return condition ? initialValue : value
+	let initialValue = useRef(value).current
+	return condition ? initialValue : value
 }
 
 function TopLevelNavItem({
-  href,
-  children,
+	href,
+	children,
 }: {
-  href: string
-  children: React.ReactNode
+	href: string
+	children: React.ReactNode
 }) {
-  return (
-    <li className="md:hidden">
-      <CloseButton
-        as={Link}
-        href={href}
-        className="block py-1 text-sm text-zinc-600 transition hover:text-zinc-900 dark:text-zinc-400 dark:hover:text-white"
-      >
-        {children}
-      </CloseButton>
-    </li>
-  )
+	return (
+		<li className="md:hidden">
+			<CloseButton
+				as={Link}
+				href={href}
+				className="block py-1 text-sm text-zinc-600 transition hover:text-zinc-900 dark:text-zinc-400 dark:hover:text-white"
+			>
+				{children}
+			</CloseButton>
+		</li>
+	)
 }
 
 function NavLink({
-  href,
-  children,
-  tag,
-  active = false,
-  isAnchorLink = false,
+	href,
+	children,
+	tag,
+	active = false,
+	isAnchorLink = false,
 }: {
-  href: string
-  children: React.ReactNode
-  tag?: string
-  active?: boolean
-  isAnchorLink?: boolean
+	href: string
+	children: React.ReactNode
+	tag?: string
+	active?: boolean
+	isAnchorLink?: boolean
 }) {
-  return (
-    <CloseButton
-      as={Link}
-      href={href}
-      aria-current={active ? 'page' : undefined}
-      className={clsx(
-        'flex justify-between gap-2 py-1 pr-3 text-sm transition',
-        isAnchorLink ? 'pl-7' : 'pl-4',
-        active
-          ? 'text-zinc-900 dark:text-white'
-          : 'text-zinc-600 hover:text-zinc-900 dark:text-zinc-400 dark:hover:text-white',
-      )}
-    >
-      <span className="truncate">{children}</span>
-      {tag && (
-        <Tag variant="small" color="zinc">
-          {tag}
-        </Tag>
-      )}
-    </CloseButton>
-  )
+	return (
+		<CloseButton
+			as={Link}
+			href={href}
+			aria-current={active ? 'page' : undefined}
+			className={clsx(
+				'flex justify-between gap-2 py-1 pr-3 text-sm transition',
+				isAnchorLink ? 'pl-7' : 'pl-4',
+				active
+					? 'text-zinc-900 dark:text-white'
+					: 'text-zinc-600 hover:text-zinc-900 dark:text-zinc-400 dark:hover:text-white',
+			)}
+		>
+			<span className="truncate">{children}</span>
+			{tag && (
+				<Tag variant="small" color="zinc">
+					{tag}
+				</Tag>
+			)}
+		</CloseButton>
+	)
 }
 
 function VisibleSectionHighlight({
-  group,
-  pathname,
+	group,
+	pathname,
 }: {
-  group: NavGroup
-  pathname: string
+	group: NavGroup
+	pathname: string
 }) {
-  let [sections, visibleSections] = useInitialValue(
-    [
-      useSectionStore((s) => s.sections),
-      useSectionStore((s) => s.visibleSections),
-    ],
-    useIsInsideMobileNavigation(),
-  )
+	let [sections, visibleSections] = useInitialValue(
+		[
+			useSectionStore((s) => s.sections),
+			useSectionStore((s) => s.visibleSections),
+		],
+		useIsInsideMobileNavigation(),
+	)
 
-  let isPresent = useIsPresent()
-  let firstVisibleSectionIndex = Math.max(
-    0,
-    [{ id: '_top' }, ...sections].findIndex(
-      (section) => section.id === visibleSections[0],
-    ),
-  )
-  let itemHeight = remToPx(2)
-  let height = isPresent
-    ? Math.max(1, visibleSections.length) * itemHeight
-    : itemHeight
-  let top =
-    group.links.findIndex((link) => link.href === pathname) * itemHeight +
-    firstVisibleSectionIndex * itemHeight
+	let isPresent = useIsPresent()
+	let firstVisibleSectionIndex = Math.max(
+		0,
+		[{ id: '_top' }, ...sections].findIndex(
+			(section) => section.id === visibleSections[0],
+		),
+	)
+	let itemHeight = remToPx(2)
+	let height = isPresent
+		? Math.max(1, visibleSections.length) * itemHeight
+		: itemHeight
+	let top =
+		group.links.findIndex((link) => link.href === pathname) * itemHeight +
+		firstVisibleSectionIndex * itemHeight
 
-  return (
-    <motion.div
-      layout
-      initial={{ opacity: 0 }}
-      animate={{ opacity: 1, transition: { delay: 0.2 } }}
-      exit={{ opacity: 0 }}
-      className="absolute inset-x-0 top-0 bg-zinc-800/2.5 will-change-transform dark:bg-white/2.5"
-      style={{ borderRadius: 8, height, top }}
-    />
-  )
+	return (
+		<motion.div
+			layout
+			initial={{ opacity: 0 }}
+			animate={{ opacity: 1, transition: { delay: 0.2 } }}
+			exit={{ opacity: 0 }}
+			className="absolute inset-x-0 top-0 bg-zinc-800/2.5 will-change-transform dark:bg-white/2.5"
+			style={{ borderRadius: 8, height, top }}
+		/>
+	)
 }
 
 function ActivePageMarker({
-  group,
-  pathname,
+	group,
+	pathname,
 }: {
-  group: NavGroup
-  pathname: string
+	group: NavGroup
+	pathname: string
 }) {
-  let itemHeight = remToPx(2)
-  let offset = remToPx(0.25)
-  let activePageIndex = group.links.findIndex((link) => link.href === pathname)
-  let top = offset + activePageIndex * itemHeight
+	let itemHeight = remToPx(2)
+	let offset = remToPx(0.25)
+	let activePageIndex = group.links.findIndex((link) => link.href === pathname)
+	let top = offset + activePageIndex * itemHeight
 
-  return (
-    <motion.div
-      layout
-      className="absolute left-2 h-6 w-px bg-cyan-500"
-      initial={{ opacity: 0 }}
-      animate={{ opacity: 1, transition: { delay: 0.2 } }}
-      exit={{ opacity: 0 }}
-      style={{ top }}
-    />
-  )
+	return (
+		<motion.div
+			layout
+			className="absolute left-2 h-6 w-px bg-cyan-500"
+			initial={{ opacity: 0 }}
+			animate={{ opacity: 1, transition: { delay: 0.2 } }}
+			exit={{ opacity: 0 }}
+			style={{ top }}
+		/>
+	)
 }
 
 function NavigationGroup({
-  group,
-  className,
+	group,
+	className,
 }: {
-  group: NavGroup
-  className?: string
+	group: NavGroup
+	className?: string
 }) {
-  // If this is the mobile navigation then we always render the initial
-  // state, so that the state does not change during the close animation.
-  // The state will still update when we re-open (re-render) the navigation.
-  let isInsideMobileNavigation = useIsInsideMobileNavigation()
-  let [pathname, sections] = useInitialValue(
-    [usePathname(), useSectionStore((s) => s.sections)],
-    isInsideMobileNavigation,
-  )
+	// If this is the mobile navigation then we always render the initial
+	// state, so that the state does not change during the close animation.
+	// The state will still update when we re-open (re-render) the navigation.
+	let isInsideMobileNavigation = useIsInsideMobileNavigation()
+	let [pathname, sections] = useInitialValue(
+		[usePathname(), useSectionStore((s) => s.sections)],
+		isInsideMobileNavigation,
+	)
 
-  let isActiveGroup =
-    group.links.findIndex((link) => link.href === pathname) !== -1
+	let isActiveGroup =
+		group.links.findIndex((link) => link.href === pathname) !== -1
 
-  return (
-    <li className={clsx('relative mt-6', className)}>
-      <motion.h2
-        layout="position"
-        className="text-xs font-semibold text-zinc-900 dark:text-white"
-      >
-        {group.title}
-      </motion.h2>
-      <div className="relative mt-3 pl-2">
-        <AnimatePresence initial={!isInsideMobileNavigation}>
-          {isActiveGroup && (
-            <VisibleSectionHighlight group={group} pathname={pathname} />
-          )}
-        </AnimatePresence>
-        <motion.div
-          layout
-          className="absolute inset-y-0 left-2 w-px bg-zinc-900/10 dark:bg-white/5"
-        />
-        <AnimatePresence initial={false}>
-          {isActiveGroup && (
-            <ActivePageMarker group={group} pathname={pathname} />
-          )}
-        </AnimatePresence>
-        <ul role="list" className="border-l border-transparent">
-          {group.links.map((link) => (
-            <motion.li key={link.href} layout="position" className="relative">
-              <NavLink href={link.href} active={link.href === pathname}>
-                {link.title}
-              </NavLink>
-              <AnimatePresence mode="popLayout" initial={false}>
-                {link.href === pathname && sections.length > 0 && (
-                  <motion.ul
-                    role="list"
-                    initial={{ opacity: 0 }}
-                    animate={{
-                      opacity: 1,
-                      transition: { delay: 0.1 },
-                    }}
-                    exit={{
-                      opacity: 0,
-                      transition: { duration: 0.15 },
-                    }}
-                  >
-                    {sections.map((section) => (
-                      <li key={section.id}>
-                        <NavLink
-                          href={`${link.href}#${section.id}`}
-                          tag={section.tag}
-                          isAnchorLink
-                        >
-                          {section.title}
-                        </NavLink>
-                      </li>
-                    ))}
-                  </motion.ul>
-                )}
-              </AnimatePresence>
-            </motion.li>
-          ))}
-        </ul>
-      </div>
-    </li>
-  )
+	return (
+		<li className={clsx('relative mt-6', className)}>
+			<motion.h2
+				layout="position"
+				className="text-xs font-semibold text-zinc-900 dark:text-white"
+			>
+				{group.title}
+			</motion.h2>
+			<div className="relative mt-3 pl-2">
+				<AnimatePresence initial={!isInsideMobileNavigation}>
+					{isActiveGroup && (
+						<VisibleSectionHighlight group={group} pathname={pathname} />
+					)}
+				</AnimatePresence>
+				<motion.div
+					layout
+					className="absolute inset-y-0 left-2 w-px bg-zinc-900/10 dark:bg-white/5"
+				/>
+				<AnimatePresence initial={false}>
+					{isActiveGroup && (
+						<ActivePageMarker group={group} pathname={pathname} />
+					)}
+				</AnimatePresence>
+				<ul role="list" className="border-l border-transparent">
+					{group.links.map((link) => (
+						<motion.li key={link.href} layout="position" className="relative">
+							<NavLink href={link.href} active={link.href === pathname}>
+								{link.title}
+							</NavLink>
+							<AnimatePresence mode="popLayout" initial={false}>
+								{link.href === pathname && sections.length > 0 && (
+									<motion.ul
+										role="list"
+										initial={{ opacity: 0 }}
+										animate={{
+											opacity: 1,
+											transition: { delay: 0.1 },
+										}}
+										exit={{
+											opacity: 0,
+											transition: { duration: 0.15 },
+										}}
+									>
+										{sections.map((section) => (
+											<li key={section.id}>
+												<NavLink
+													href={`${link.href}#${section.id}`}
+													tag={section.tag}
+													isAnchorLink
+												>
+													{section.title}
+												</NavLink>
+											</li>
+										))}
+									</motion.ul>
+								)}
+							</AnimatePresence>
+						</motion.li>
+					))}
+				</ul>
+			</div>
+		</li>
+	)
 }
 
 export const navigation: Array<NavGroup> = [
-  {
-    title: 'Installation',
-    links: [
-      { title: 'Introduction', href: '/' },
-      { title: 'Operating System Support', href: '/os-support' },
-      { title: 'Install from repository', href: '/install/repo' },
-      { title: 'Install from script', href: '/install/script' },
-      { title: 'Build from source', href: '/build' },
-      { title: 'Nixos', href: '/nixos' },
-    ],
-  },
-  {
-    title: 'Quickstart',
-    links: [
-      { title: 'Hyprland Quickstart', href: '/quickstart/hyprland' },
-      { title: 'Gnome Quickstart', href: '/quickstart/gnome' },
-      { title: 'KDE Quickstart', href: '/quickstart/kde' },
-      { title: 'Cosmic Desktop Quickstart', href: '/quickstart/cosmic' },
-      { title: 'Niri Quickstart', href: '/quickstart/niri' },
-      { title: 'General Quickstart', href: '/quickstart/generic' },
-    ],
-  },
+	{
+		title: 'Installation',
+		links: [
+			{ title: 'Introduction', href: '/' },
+			{ title: 'Operating System Support', href: '/os-support' },
+			{ title: 'Install from repository', href: '/install/repo' },
+			{ title: 'Install from script', href: '/install/script' },
+			{ title: 'Build from source', href: '/build' },
+			{ title: 'Nixos', href: '/nixos' },
+		],
+	},
+	{
+		title: 'Quickstart',
+		links: [
+			{ title: 'Hyprland Quickstart', href: '/quickstart/hyprland' },
+			{ title: 'Gnome Quickstart', href: '/quickstart/gnome' },
+			{ title: 'KDE Quickstart', href: '/quickstart/kde' },
+			{ title: 'Cosmic Desktop Quickstart', href: '/quickstart/cosmic' },
+			{ title: 'Niri Quickstart', href: '/quickstart/niri' },
+			{ title: 'General Quickstart', href: '/quickstart/generic' },
+		],
+	},
 
-  {
-    title: 'Manual',
-    links: [
-      { title: 'Configuration', href: '/config' },
-      { title: 'Known Caveats', href: '/caveats' },
-      { title: 'Network and Privacy', href: '/privacy' },
-      { title: 'Launcher Window', href: '/launcher-window' },
-      { title: 'DMenu Mode', href: '/dmenu' },
-      { title: 'Clipboard Management', href: '/clipboard' },
-      { title: 'Window Management', href: '/window' },
-      { title: 'Calculator', href: '/calculator' },
-      { title: 'Application Shortcuts', href: '/shortcuts' },
-      { title: 'File Search', href: '/file-search' },
-      { title: 'Fallback Commands', href: '/fallback-commands' },
-      { title: 'Extensions', href: '/install-extensions' },
-      { title: 'Emoji Picker', href: '/emoji' },
-      { title: 'Deeplinks', href: '/deeplinks' },
-      { title: 'Favicon Loading', href: '/favicon' },
-      { title: 'Set Default Terminal', href: '/default-terminal' },
-    ],
-  },
-  {
-    title: 'Theming',
-    links: [
-      { title: 'Getting Started', href: '/theming/getting-started' },
-      { title: 'Theme Structure', href: '/theming/theme-structure' },
-      { title: 'Advanced Features', href: '/theming/advanced-features' },
-      { title: 'CLI Tools', href: '/theming/cli-tools' },
-      { title: 'Matugen integration', href: '/theming/matugen' },
-    ],
-  },
-  {
-    title: 'Extensions',
-    links: [
-      { title: 'Introduction', href: '/extensions/introduction' },
-      { title: 'Create Your First Extension', href: '/extensions/create' },
-      { title: 'File Structure', href: '/extensions/file-structure' },
-      { title: 'Manifest', href: '/extensions/manifest' },
-      { title: 'Debug Raycast Extensions', href: '/extensions/debug-raycast' },
-      {
-        title: 'API Reference',
-        href: 'https://github.com/vicinaehq/vicinae/tree/main/typescript/api/docs',
-      },
-      { title: 'Create a view command', href: '/extensions/view-command' },
-      {
-        title: 'Create a no-view command',
-        href: '/extensions/no-view-command',
-      },
-    ],
-  },
+	{
+		title: 'Manual',
+		links: [
+			{ title: 'Configuration', href: '/config' },
+			{ title: 'Known Caveats', href: '/caveats' },
+			{ title: 'Network and Privacy', href: '/privacy' },
+			{ title: 'Launcher Window', href: '/launcher-window' },
+			{ title: 'dmenu Mode', href: '/dmenu' },
+			{ title: 'Clipboard Management', href: '/clipboard' },
+			{ title: 'Window Management', href: '/window' },
+			{ title: 'Calculator', href: '/calculator' },
+			{ title: 'Application Shortcuts', href: '/shortcuts' },
+			{ title: 'File Search', href: '/file-search' },
+			{ title: 'Fallback Commands', href: '/fallback-commands' },
+			{ title: 'Extensions', href: '/install-extensions' },
+			{ title: 'Emoji Picker', href: '/emoji' },
+			{ title: 'Deeplinks', href: '/deeplinks' },
+			{ title: 'Favicon Loading', href: '/favicon' },
+			{ title: 'Set Default Terminal', href: '/default-terminal' },
+		],
+	},
+	{
+		title: 'Theming',
+		links: [
+			{ title: 'Getting Started', href: '/theming/getting-started' },
+			{ title: 'Theme Structure', href: '/theming/theme-structure' },
+			{ title: 'Advanced Features', href: '/theming/advanced-features' },
+			{ title: 'CLI Tools', href: '/theming/cli-tools' },
+			{ title: 'Matugen integration', href: '/theming/matugen' },
+		],
+	},
+	{
+		title: 'Extensions',
+		links: [
+			{ title: 'Introduction', href: '/extensions/introduction' },
+			{ title: 'Create Your First Extension', href: '/extensions/create' },
+			{ title: 'File Structure', href: '/extensions/file-structure' },
+			{ title: 'Manifest', href: '/extensions/manifest' },
+			{ title: 'Debug Raycast Extensions', href: '/extensions/debug-raycast' },
+			{
+				title: 'API Reference',
+				href: 'https://github.com/vicinaehq/vicinae/tree/main/typescript/api/docs',
+			},
+			{ title: 'Create a view command', href: '/extensions/view-command' },
+			{
+				title: 'Create a no-view command',
+				href: '/extensions/no-view-command',
+			},
+		],
+	},
 
-  {
-    title: 'Contributing',
-    links: [
-      { title: 'Clipboard Server', href: '/contrib/clipboard' },
-      { title: 'Documentation', href: '/contrib/docs' },
-    ],
-  },
+	{
+		title: 'Contributing',
+		links: [
+			{ title: 'Clipboard Server', href: '/contrib/clipboard' },
+			{ title: 'Documentation', href: '/contrib/docs' },
+		],
+	},
 ]
 
 export function Navigation(props: React.ComponentPropsWithoutRef<'nav'>) {
-  return (
-    <nav {...props}>
-      <ul role="list">
-        {navigation.map((group, groupIndex) => (
-          <NavigationGroup
-            key={group.title}
-            group={group}
-            className={groupIndex === 0 ? 'md:mt-0' : ''}
-          />
-        ))}
-        <li className="sticky bottom-0 z-10 mt-6 min-[416px]:hidden">
-          <Button href="#" variant="filled" className="w-full">
-            Sign in
-          </Button>
-        </li>
-      </ul>
-    </nav>
-  )
+	return (
+		<nav {...props}>
+			<ul role="list">
+				{navigation.map((group, groupIndex) => (
+					<NavigationGroup
+						key={group.title}
+						group={group}
+						className={groupIndex === 0 ? 'md:mt-0' : ''}
+					/>
+				))}
+				<li className="sticky bottom-0 z-10 mt-6 min-[416px]:hidden">
+					<Button href="#" variant="filled" className="w-full">
+						Sign in
+					</Button>
+				</li>
+			</ul>
+		</nav>
+	)
 }
